d22904cafce33872acbafca15abf2f83fefe07d0
Luana <santos.luanabeatriz1995@gmail.com>
2024-07-12 22:18:58 +0100

    Update CardFreeTrail.jsx

--- src/components/Molecule/CardFreeTrail/CardFreeTrail.jsx
+++ src/components/Molecule/CardFreeTrail/CardFreeTrail.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function CardFreeTrail() {
     return (
         <div className='w-full pb-32 bg-black-08 flex'>
-            <div className='bg-[url("/images/background_card.png")] h-80 w-full flex items-center justify-between px-20 gap-14 lg_4:flex-col lg_4:justify-center'>
+            <div className='bg-[url("/images/background_card.png")] h-80 w-full flex items-center justify-between px-20 gap-14 lg_4:flex-col lg_4:justify-center lg_4:px-5'>
                 <div className='flex-1 lg_4:flex-none'>
                     <h1 className='text-2xl mb-2'>Start your free trial today!</h1>
                     <p>This is a clear and concise call to action that encourages users to sign up for a free trial of StreamVibe.</p>
