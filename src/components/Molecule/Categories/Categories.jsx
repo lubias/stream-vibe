fc2690cd64c330116eb2522f2903d59c17e84efb
Luana <santos.luanabeatriz1995@gmail.com>
2024-07-12 22:53:30 +0100

    changed images from svg to png

--- src/components/Molecule/Categories/Categories.jsx
+++ src/components/Molecule/Categories/Categories.jsx
@@ -14,11 +14,11 @@ function Categories() {
                 <p className='text-xl'>Whether you're looking for a comedy to make you laugh, a drama to make you think, or a documentary to learn something new</p>
             </div>
             <div className='flex gap-7 justify-between lg_4:hidden'>
-                <CardCategories image='images/cards/action.svg' genre="Action" />
-                <CardCategories image='images/cards/adventure.svg' genre="Adventure" />
-                <CardCategories image='images/cards/comedy.svg' genre="Comedy" />
-                <CardCategories image='images/cards/drama.svg' genre="Drama" />
-                <CardCategories image='images/cards/horror.svg' genre="Horror" />
+                <CardCategories image='images/cards/action.png' genre="Action" />
+                <CardCategories image='images/cards/adventure.png' genre="Adventure" />
+                <CardCategories image='images/cards/comedy.png' genre="Comedy" />
+                <CardCategories image='images/cards/drama.png' genre="Drama" />
+                <CardCategories image='images/cards/horror.png' genre="Horror" />
             </div>
             <div className='hidden lg_4:block'>
                 <Swiper
@@ -30,11 +30,11 @@ function Categories() {
                     modules={[Scrollbar]}
                     className="mySwiper"
                 >
-                    <SwiperSlide><CardCategories image='images/cards/action.svg' genre="Action" /></SwiperSlide>
-                    <SwiperSlide><CardCategories image='images/cards/adventure.svg' genre="Adventure" /></SwiperSlide>
-                    <SwiperSlide><CardCategories image='images/cards/comedy.svg' genre="Comedy" /></SwiperSlide>
-                    <SwiperSlide><CardCategories image='images/cards/drama.svg' genre="Drama" /></SwiperSlide>
-                    <SwiperSlide><CardCategories image='images/cards/horror.svg' genre="Horror" /></SwiperSlide>
+                    <SwiperSlide><CardCategories image='images/cards/action.png' genre="Action" /></SwiperSlide>
+                    <SwiperSlide><CardCategories image='images/cards/adventure.png' genre="Adventure" /></SwiperSlide>
+                    <SwiperSlide><CardCategories image='images/cards/comedy.png' genre="Comedy" /></SwiperSlide>
+                    <SwiperSlide><CardCategories image='images/cards/drama.png' genre="Drama" /></SwiperSlide>
+                    <SwiperSlide><CardCategories image='images/cards/horror.png' genre="Horror" /></SwiperSlide>
                 </Swiper>
             </div>
         </div>
