b1c4171dda529660a17ea7eee561b77028b73d9e
Luana <santos.luanabeatriz1995@gmail.com>
2024-07-12 22:08:01 +0100

    update route / to open /home

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // next.config.js
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/home',
+                permanent: true,
+            },
+        ]
+    },
+};
 
 export default nextConfig;
